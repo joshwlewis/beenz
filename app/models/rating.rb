f6ab5401a082b98269f0de3a70233e5e4cf5a040
Josh Lewis <josh.w.lewis@gmail.com>
2014-10-29 11:51:48 -0500

    Update beenz on updated ratings.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,8 @@ class Rating < ActiveRecord::Base
   belongs_to :rater, class_name: 'User'
   belongs_to :ratee, class_name: 'User'
 
+  after_save :update_ratee_beenz
+
   validates_presence_of :rater, :ratee, :beenz
   validates_associated :rater, :ratee
 
@@ -9,4 +11,10 @@ class Rating < ActiveRecord::Base
 
   validates_numericality_of :beenz, integer_only: true,
     greater_than_or_equal_to: 1, less_than_or_equal_to: 5
+
+  private
+
+  def update_ratee_beenz
+    ratee.update_beenz!
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,33 @@ class User < ActiveRecord::Base
   validates_numericality_of :beenz, integer_only: true,
     greater_than_or_equal_to: 1, less_than_or_equal_to: 5
 
+  def calculate_beenz
+    total_rating = 0
+    total_weight  = 0
+    recieved_ratings.includes(:rater).each do |rating|
+      weight = rating.rater.beenz
+      rating = rating.beenz * weight
+      total_weight += weight
+      total_rating += rating
+    end
+    case (total_rating.to_f / total_weight)
+    when 1..1.8
+      1
+    when 1.8..2.6
+      2
+    when 2.6..3.4
+      3
+    when 3.4..4.2
+      4
+    when 4.2..5
+      5
+    end
+  end
+
+  def update_beenz!
+    self.update_attributes(beenz: calculate_beenz)
+  end
+
   def email=(string)
     super(string.strip.downcase)
   end
